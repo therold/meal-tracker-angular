42edcdb0d697cf6a42a1d6dfe07c46cdc0d8945c
Timothy Herold <heroldt@gmail.com>
2016-10-23 13:14:35 -0700

    Add list of foods to dayInfo model.

--- app/dayInfo.model.ts
+++ app/dayInfo.model.ts
@@ -1,5 +1,18 @@
-export class dayInfo {
-  public date: Date;
-  public totalCalories: number;
-  public averageCalories: number;
+import { Food } from './food.model';
+
+export class DayInfo {
+
+  constructor(public date: Date, public foods: Food[]) { }
+
+  getTotalCalories(): number {
+    var total: number = 0;
+    for(var food of this.foods) {
+      total += food.calories;
+    }
+    return total;
+  }
+
+  getAverageCalories(): number {
+    return this.getTotalCalories() / this.foods.length || 0 ;
+  }
 }
